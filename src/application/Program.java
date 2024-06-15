c2d007b6465af6d19e84f7a237cedea716bb94fe
Danilo Brandão <danilo.brandao.ifpr@gmail.com>
2024-06-14 23:59:50 -0300

    findById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,10 +12,9 @@ public class Program {
 	
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "books");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(1, "Dudão", "dudu@gmail.com", new Date(), 3000.0, obj);
-		
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
 	}
