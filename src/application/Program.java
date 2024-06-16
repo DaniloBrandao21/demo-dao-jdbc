b1f1ae6c7b89c0f76af348140cdafede5301aea6
Danilo Brandão <danilo.brandao.ifpr@gmail.com>
2024-06-16 02:51:56 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,6 +15,7 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Department department = new Department(2, null);
 		System.out.println("===== Test 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
@@ -31,9 +32,14 @@ public class Program {
 		System.out.println("\n===== Test 3: seller findAll =====");
 		List<Seller> listFindAll = sellerDao.findAll();
 		
-		for( Seller sel: listFindAll) {
-			System.out.println(sel);
-		}
+		System.out.println("\n===== Test 4: seller insert =====");
+		
+		Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		
 		
 		
 	}
